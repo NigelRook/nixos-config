500569d708bcc8c827cb11f64b9067648cfcf49c
Nigel Rook <nigelrook@gmail.com>
2024-07-22 23:09:14 +0100

    Add ack and jq

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,8 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
+    jq
+    ack
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

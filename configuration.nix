c8c64bac085c034952cea13953ebeac02f14fec3
Nigel Rook <nigelrook@gmail.com>
2024-07-22 23:03:33 +0100

    Fix hostname

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "helmut"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary

f66032b5b236cf784dcfa7cf18717c4f3f6b892b
Nigel Rook <nigelrook@gmail.com>
2024-08-03 19:43:18 +0100

    Make home-manager config per-host

--- home/flake.nix
+++ home/flake.nix
@@ -16,7 +16,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."nigel" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."nigel@helmut" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,

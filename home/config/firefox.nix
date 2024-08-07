c01413ecd8b58822557c2b398c986ba3224b6763
Nigel Rook <nigelrook@gmail.com>
2024-08-07 19:58:21 +0100

    Update firefox settings

--- home/config/firefox.nix
+++ home/config/firefox.nix
@@ -19,7 +19,13 @@
       settings = {
         "apz.fling_friction" = 0.005;
         "apz.fling_min_velocity_threshold" = 1.5;
+        "browser.sessionstore.max_resumed_crashes" = -1;
         "signon.rememberSignons" = false;
+        "extensions.formautofill.creditCards.enabled" = false;
+        "privacy.donottrackheader.enabled" = true;
+        "services.sync.engine.addons" = false;
+        "services.sync.engine.bookmarks" = false;
+        "services.sync.engine.prefs" = false;
       };
     };
   };

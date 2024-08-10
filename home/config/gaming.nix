0bf23b639a837e412dcd1c8f7d5dd6acbd76f74d
Nigel Rook <nigelrook@gmail.com>
2024-08-10 13:44:51 +0100

    Set -platform wayland for Moonlight

--- home/config/gaming.nix
+++ home/config/gaming.nix
@@ -4,4 +4,17 @@
     discord
     moonlight-qt
   ];
+
+  # Override moonlight .desktop to add -platform wayland (needed for hardware decoding)
+  xdg.desktopEntries."com.moonlight_stream.Moonlight.desktop" = {
+    name = "Moonlight";
+    comment = "Stream games and other applications from another PC running Sunshine or GeForce Experience";
+    exec = "moonlight -platform wayland";
+    icon = "moonlight";
+    terminal = false;
+    categories = [ "Qt" "Game" ];
+    settings = {
+      Keywords = "nvidia;gamestream;stream;sunshine;remote play;";
+    };
+  };
 }

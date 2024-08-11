370cdb346a29646c196fa66684abbac60b44e047
Nigel Rook <nigelrook@gmail.com>
2024-08-11 18:14:21 +0100

    Refactor home-manager flake

--- home/flake.nix
+++ home/flake.nix
@@ -13,24 +13,35 @@
 
   outputs = { nixpkgs, home-manager, nur, ... }:
     {
-      homeConfigurations."nigel@helmut" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      homeConfigurations =
+      let
+        configDef = userHost: modules:
+          let
+            lib = nixpkgs.lib;
+            parts = lib.strings.splitString "@" userHost;
+            user = lib.lists.elemAt parts 0;
+          in
+          home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages."x86_64-linux";
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          ./users/nigel.nix
-          { nixpkgs.overlays = [ nur.overlay ]; }
-          { nixpkgs.config.allowUnfree = true; }
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+
+            modules = [
+              { nixpkgs.overlays = [ nur.overlay ]; }
+              { nixpkgs.config.allowUnfree = true; }
+              ./users/${user}.nix
+            ] ++ modules;
+          };
+      in
+      builtins.mapAttrs configDef {
+        "nigel@helmut" = [
           ./config/common.nix
           ./config/gnome-desktop.nix
           ./config/firefox.nix
           ./config/dev.nix
           ./config/gaming.nix
         ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }

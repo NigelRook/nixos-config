cd386769cc6b8b9e6616ede24bc7fd66ca31f34e
Nigel Rook <nigelrook@gmail.com>
2024-09-24 19:14:10 +0100

    Extend virtualization config

--- home/config/dev.nix
+++ home/config/dev.nix
@@ -31,4 +31,8 @@
       defaultBranch = "main";
     };
   };
+
+  home.file."~/.config/libvirt/qemu.conf".text = ''
+    nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
+  '';
 }
--- system/config/dev.nix
+++ system/config/dev.nix
@@ -1,4 +1,22 @@
+{ pkgs, ... }:
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
   programs.virt-manager.enable = true;
+  users.users.nigel = {
+    extraGroups = [ "libvirtd" ];
+  };
 }

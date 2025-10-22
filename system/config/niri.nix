d2fd1ba099893215d9f05f9f37653102a22d8b00
Nigel Rook <nigelrook@gmail.com>
2025-10-22 23:04:41 +0100

    Fix double waybar

--- system/config/niri.nix
+++ system/config/niri.nix
@@ -8,7 +8,6 @@
   security.pam.services.swaylock = {};
   services.gnome.gnome-keyring.enable = true; # secret service
 
-  programs.waybar.enable = true; # top bar
   environment.systemPackages = with pkgs; [
     ptyxis
     fuzzel
@@ -16,6 +15,7 @@
     swayidle
     swaybg
     mako
+    waybar
     blanket
   ];
 }

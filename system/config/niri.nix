ffdc0acf0bdeddfc1c42cd24f56868631c7ec106
Nigel Rook <nigelrook@gmail.com>
2025-10-25 13:01:26 +0100

    Noctalia

--- system/config/niri.nix
+++ system/config/niri.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, inputs, ... }:
 {
   services.displayManager.gdm.enable = true;
 
@@ -8,15 +8,43 @@
   security.pam.services.swaylock = {};
   services.gnome.gnome-keyring.enable = true; # secret service
 
+  security.pam.services.login.fprintAuth = lib.mkForce true;
+  # systemd.user.services.swaybg = {
+  #   enable = true;
+
+  #   partOf = "graphical-session.target";
+  #   after = "graphical-session.target";
+  #   requisite = [ "graphical-session.target" "niri.service" ];
+
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.swaybg}/bin/swaybg -m fill -i ${../files/wallpaper.png}";
+  #     Restart = "on-failure";
+  #   };
+  # };
+
+  # systemd.user.services.swayidle = {
+  #   enable = true;
+
+  #   partOf = "graphical-session.target";
+  #   after = "graphical-session.target";
+  #   requisite = [ "graphical-session.target" "niri.service" ];
+
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.swaybg}/bin/swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f'";
+  #     Restart = "on-failure";
+  #   };
+  # };
+
   environment.systemPackages = with pkgs; [
+    inputs.noctalia.packages.${system}.default
     ptyxis
-    fuzzel
+    # fuzzel
     swaylock
-    swayidle
-    swaybg
-    mako
-    waybar
+    # swaybg
+    # mako
+    # waybar
     blanket
+    xwayland-satellite
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
--- system/flake.nix
+++ system/flake.nix
@@ -17,6 +17,15 @@
       url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
+    };
   };
 
   outputs = { self, nixpkgs, deploy-rs, nixos-hardware, lanzaboote, disko, ... }@inputs: {

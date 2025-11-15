44056497ddbffdd0a60541a670b53beae64b7228
Nigel Rook <nigelrook@gmail.com>
2025-11-15 23:32:33 +0000

    Fix argocd namespace

--- system/config/kubernetes-resources.nix
+++ system/config/kubernetes-resources.nix
@@ -22,13 +22,23 @@
         };
       };
       cloudflare-api-token-secret.source = config.sops.templates."cloudflare-token.yaml".path;
+
+      argocd-namespace.content = {
+        apiVersion = "v1";
+        kind = "Namespace";
+        metadata = {
+          name = "argocd";
+        };
+      };
     };
+
     autoDeployCharts = {
       argocd = {
         name = "argo-cd";
         repo = "https://argoproj.github.io/argo-helm";
         version = "9.1.3";
         hash = "sha256-OG74wEZuXyqT5S98lhj/E+t+KScJZycVWeLORPs8J7I=";
+        targetNamespace = "argocd";
         values = {
           configs = {
             params = {

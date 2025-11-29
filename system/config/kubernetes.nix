34a25784761a98c757de0cdd7ba2aac425332cc1
Nigel Rook <nigelrook@gmail.com>
2025-11-29 17:39:31 +0000

    Add openiscsi for k8s boxes

--- system/config/kubernetes.nix
+++ system/config/kubernetes.nix
@@ -5,6 +5,8 @@
     gracefulNodeShutdown.enable = true;
   };
 
+  services.openiscsi.enable = true;
+
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration

eccca459e202a5cd17a38cef863af2ec87539758
Nigel Rook <nigelrook@gmail.com>
2026-01-23 19:43:16 +0000

    k8s longhorn support

--- system/config/kubernetes.nix
+++ system/config/kubernetes.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   services.k3s = {
     enable = true;
     gracefulNodeShutdown.enable = true;
   };
 
-  services.openiscsi.enable = true;
+  services.openiscsi = {
+    enable = true;
+    name = "iqn.2016-04.com.open-iscsi:${config.networking.hostName}";
+  };
+
+  system.activationScripts.longhorn-fix = ''
+    mkdir -p /usr/bin
+    ln -sf /run/current-system/sw/bin/iscsiadm /usr/bin/iscsiadm
+    ln -sf /run/current-system/sw/bin/mount /usr/bin/mount
+    ln -sf /run/current-system/sw/bin/blkid /usr/bin/blkid
+  '';
+
+  boot.kernelModules = [ "iscsi_tcp" ];
 
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
@@ -16,5 +28,9 @@
     # 8472 # k3s, flannel: required if using multi-node for inter-node networking
   ];
 
-  environment.systemPackages = [ pkgs.kubectx ];
+  environment.systemPackages = with pkgs; [
+    util-linux
+    nfs-utils#
+    kubectx
+  ];
 }

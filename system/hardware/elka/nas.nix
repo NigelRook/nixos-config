4ea88d54455927a9c455ffdbc3e6efaacf532254
Nigel Rook <nigelrook@gmail.com>
2026-02-07 00:53:46 +0000

    Add nfs server

--- system/hardware/elka/nas.nix
+++ system/hardware/elka/nas.nix
@@ -25,4 +25,19 @@
       "nofail"
     ];
   };
+
+  fileSystems."/export/media" = {
+    device = "/srv/data/media";
+    options = [ "bind" "nofail" ];
+  };
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /export       192.168.2.0/24(fsid=0,crossmnt)
+      /export/media 192.168.2.0/24(fsid=1,rw,sync,no_wdelay,insecure,anonuid=1000,anongid=1000,all_squash)
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [ 2049 ];
 }

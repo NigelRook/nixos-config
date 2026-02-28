1b6e5b56208dab425e97cc73c1869d214c11bb61
Nigel Rook <nigelrook@gmail.com>
2026-02-28 23:38:33 +0000

    Update

--- system/flake.nix
+++ system/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-unstable branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/a479ce742b98a137403ae58184f6f947aa8e6fa0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";

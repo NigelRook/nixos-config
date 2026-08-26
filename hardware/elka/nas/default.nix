a0f85cd548774ef59cc02b3b892d4442758ca98d
Nigel Rook <nigelrook@gmail.com>
2026-08-27 00:25:56 +0100

    Update

--- hardware/elka/nas/default.nix
+++ hardware/elka/nas/default.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   imports = [
     ./filesystems.nix
@@ -9,4 +10,8 @@
   services.k3s.nodeLabel = [
     "nas=true"
   ];
+
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 244 /dev/%k"
+  '';
 }
